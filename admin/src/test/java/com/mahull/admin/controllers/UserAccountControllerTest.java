e1310c4eae8f5d4637b7545eeb7f78e51d3c875a
chigozie <ucugo@yahoo.co.uk>
2016-04-08 23:56:25 +0100

    Added some unit tests for UserAccountController

---
+++ admin/src/test/java/com/mahull/admin/controllers/UserAccountControllerTest.java
@@ -0,0 +1,18 @@
+package com.mahull.admin.controllers;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Ugo on 07/04/2016.
+ */
+public class UserAccountControllerTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testAddNewUser() throws Exception {
+
+    }
+}
