2adcb97b0216f23dfed6c3ec75b4642cd515e9b4
chigozie <ucugo@yahoo.co.uk>
2016-04-09 00:36:03 +0100

    Added some unit tests for BaseController

---
+++ admin/src/test/java/com/mahull/admin/controllers/BaseControllerTest.java
@@ -0,0 +1,36 @@
+package com.mahull.admin.controllers;
+
+import com.mahull.model.model.CraftUser;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestAttributes;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by Ugo on 09/04/2016.
+ */
+public class BaseControllerTest {
+
+    @Test
+    public void shouldReturnExistingCraftUserFromSession() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.getSession().setAttribute(CraftUser.REQUEST_SCOPED_ATTRIBUTE_NAME, RequestAttributes.SCOPE_SESSION);
+        CraftUser craftUser = new StubBaseController().getCraftUser(request);
+
+        assertThat(!craftUser.isNew());
+    }
+
+    @Test
+    public void shouldCreateANewCraftUser(){
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        CraftUser craftUser = new StubBaseController().getCraftUser(request);
+
+        assertThat(craftUser.isNew());
+    }
+
+    private class StubBaseController extends BaseController {
+
+    }
+
+}
